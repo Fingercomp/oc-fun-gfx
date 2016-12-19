b9d3d539a5dbaa005fe0ad660432a3b5c9b4235a
Fingercomp <fingercomp@users.noreply.github.com>
2016-12-19 17:47:51 +0700

    Update the example

--- charts/examples/hist-example-1.lua
+++ charts/examples/hist-example-1.lua
@@ -2,15 +2,16 @@ local charts = require("charts")
 
 local container = charts.Container()
 local payload = charts.Histogram()
-payload.max = 80
-payload.align = sides.RIGHT
+payload.max = 100
+payload.min = -100
+payload.align = charts.sides.RIGHT
 payload.colorFunc = function(index, perc, value, self, container)
-  return 0x20ff20
+  return 0xafff20
 end
 container.payload = payload
 
 for i = 1, 400, 1 do
-  table.insert(payload.values, math.random(0, 80))
+  table.insert(payload.values, math.sin(math.rad(i * 5)) * 100)
   container:draw()
 
   os.sleep(.05)
