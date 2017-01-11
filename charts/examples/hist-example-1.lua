e33e1115fb89f10b060d2949f0a9bbdbde84ce4b
Fingercomp <fingercomp@users.noreply.github.com>
2017-01-11 13:35:18 +0700

    Update the example

--- charts/examples/hist-example-1.lua
+++ charts/examples/hist-example-1.lua
@@ -1,10 +1,13 @@
-local charts = dofile("charts.lua")
+local charts = require("charts")
 local event = require("event")
+local gpu = require("component").gpu
 
 local container = charts.Container()
+container.width, container.height = gpu.getViewport()
+
 local payload = charts.Histogram()
-payload.max = 100
-payload.min = -100
+payload.max = 1
+payload.min = -1
 payload.level.value = 0
 payload.level.y = .5
 payload.align = charts.sides.RIGHT
@@ -13,8 +16,11 @@ payload.colorFunc = function(index, perc, value, self, container)
 end
 container.payload = payload
 
-for i = 1, 400, 1 do
-  table.insert(payload.values, math.sin(math.rad(i * 5)) * 100)
+for i = 1, math.huge, 1 do
+  table.insert(payload.values, math.sin(math.rad(i * 3)))
+  if #payload.values > container.width then
+    table.remove(payload.values, 1)
+  end
   container:draw()
 
   if event.pull(.05, "interrupted") then
