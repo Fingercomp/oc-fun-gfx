ba3cf470a0b0a58d8e2bccc7c22d734f17df4a40
Fingercomp <fingercomp@users.noreply.github.com>
2017-01-24 23:59:00 +0700

    Show send traffic

--- sniff/sniff.lua
+++ sniff/sniff.lua
@@ -2,7 +2,7 @@ local com = require("component")
 local event = require("event")
 local term = require("term")
 local comp = require("computer")
-local forms=require("forms")
+local forms = require("forms")
 
 local gpu = com.gpu
 
@@ -15,25 +15,25 @@ gpu.setResolution(80, 25)
 forms.ignoreAll()
 local elements = {}
 
-local main=forms.addForm()
-main.color=1973790
+local main = forms.addForm()
+main.color = 0x333333
 elements.main = main
 
-local topFrame=main:addFrame(1,1,0)
-topFrame.W=80
-topFrame.H=1
-topFrame.color=0xcccccc
-topFrame.fontColor=0
+local topFrame = main:addFrame(1,1,0)
+topFrame.W = 80
+topFrame.H = 1
+topFrame.color = 0xCCCCCC
+topFrame.fontColor = 0
 elements.topFrame = topFrame
 
-local appName=topFrame:addLabel(3,1,"NETWORK SNIFFER")
-appName.color=0xcccccc
-appName.fontColor=0
-elements.appName=appName
+local appName = topFrame:addLabel(3,1,"NETWORK SNIFFER")
+appName.color = 0xCCCCCC
+appName.fontColor = 0
+elements.appName = appName
 
 local function updateMsgData()
   local self = elements.msgList
-  elements.modemAddr.caption = "MODEM:    " .. self.items[self.index][1]
+  elements.recvAddr.caption = "RECEIVER: " .. self.items[self.index][1]
   elements.sendAddr.caption =  "SENDER:   " .. self.items[self.index][2]
   elements.distance.caption =  "DISTANCE: " .. self.items[self.index][4]
   elements.port.caption =      "PORT:     " .. self.items[self.index][3]
@@ -46,65 +46,65 @@ local function updateMsgData()
   end
   main:redraw()
 end
-local msgList=main:addList(1,2,updateMsgData)
-msgList.sfColor=0
-msgList.selColor=16777215
-msgList.color=3355443
-msgList.border=0
-msgList.W=80
-msgList.H=9
+local msgList = main:addList(1,2,updateMsgData)
+msgList.sfColor = 0x000000
+msgList.selColor = 0xFFFFFF
+msgList.color = 0x333333
+msgList.border = 0
+msgList.W = 80
+msgList.H = 9
 elements.msgList = msgList
 
-local msgInfo=main:addFrame(1,11,0)
-msgInfo.H=15
-msgInfo.color=12829635
-msgInfo.W=80
+local msgInfo = main:addFrame(1,11,0)
+msgInfo.H = 15
+msgInfo.color = 0xCCCCCC
+msgInfo.W = 80
 msgInfo:hide()
 elements.msgInfo = msgInfo
 
-local modemAddr=msgInfo:addLabel(3,2,"MODEM: ")
-modemAddr.fontColor=0
-modemAddr.color=12829635
-modemAddr.W=7
-elements.modemAddr = modemAddr
+local recvAddr = msgInfo:addLabel(3,2,"RECEIVER: ")
+recvAddr.fontColor = 0x00000
+recvAddr.color = 0xCCCCCC
+recvAddr.W = 7
+elements.recvAddr = recvAddr
 
-local sendAddr=msgInfo:addLabel(3,3,"SENDER: ")
-sendAddr.fontColor=0
-sendAddr.color=12829635
-sendAddr.W=7
+local sendAddr = msgInfo:addLabel(3,3,"SENDER: ")
+sendAddr.fontColor = 0x00000
+sendAddr.color = 0xCCCCCC
+sendAddr.W = 7
 elements.sendAddr = sendAddr
 
-local distance=msgInfo:addLabel(3,4,"DISTANCE: ")
-distance.fontColor=0
-distance.color=12829635
-distance.W=10
+local distance = msgInfo:addLabel(3,4,"DISTANCE: ")
+distance.fontColor = 0x00000
+distance.color = 0xCCCCCC
+distance.W = 10
 elements.distance = distance
 
-local port=msgInfo:addLabel(3,5,"PORT: ")
-port.fontColor=0
-port.color=12829635
-port.W=6
+local port = msgInfo:addLabel(3,5,"PORT: ")
+port.fontColor = 0x00000
+port.color = 0xCCCCCC
+port.W = 6
 elements.port = port
 
-local data=msgInfo:addEdit(3,6)
-data.H=10
-data.fontColor=0
-data.color=13421772
-data.W=72
+local data = msgInfo:addEdit(3,6)
+data.H = 10
+data.fontColor = 0x00000
+data.color = 0xCCCCCC
+data.W = 72
 elements.data = data
 
-local chunkList=msgInfo:addList(74,6,function()
+local chunkList = msgInfo:addList(74,6,function()
   local self = elements.chunkList
   elements.data.text = self.items[self.index]
   main:redraw()
 end)
-chunkList.sfColor=0
-chunkList.H=10
-chunkList.selColor=16777215
-chunkList.fontColor=0
-chunkList.border=1
-chunkList.color=0xcccccc
-chunkList.W=7
+chunkList.sfColor = 0
+chunkList.H = 10
+chunkList.selColor = 0xFFFFFF
+chunkList.fontColor = 0x00000
+chunkList.border = 1
+chunkList.color = 0xCCCCCC
+chunkList.W = 7
 elements.chunkList = chunkList
 
 local function modemListener(name, recv, send, port, dist, ...)
@@ -121,7 +121,45 @@ local quitListener = main:addEvent("interrupted", function()
 end)
 
 event.listen("modem_message", modemListener)
+
+local invoke = com.invoke
+com.invoke = function(address, method, ...)
+  if com.type(address) == "modem" then
+    local modem = com.proxy(address)
+    if method == "send" then
+      local result = {invoke(address, "send", ...)}
+      local args = {...}
+      local addr = table.remove(args, 1)
+      local port = table.remove(args, 1)
+      local distance = modem.isWireless() and modem.getStrength() or 0
+      elements.msgList:insert(
+        ("[" .. ("%10.2f"):format(comp.uptime()) .. "] #" .. ("%5d"):format(port) ..
+        " " .. modem.address:sub(1, 8) .. "… → " .. addr:sub(1, 8) .. "…"),
+        {addr, modem.address, port, distance, table.unpack(args)})
+      msgInfo:show()
+      updateMsgData()
+      return table.unpack(result)
+    elseif method == "broadcast" then
+      local result = {invoke(address, "broadcast", ...)}
+      local args = {...}
+      local port = table.remove(args, 1)
+      local distance = modem.isWireless() and modem.getStrength() or 0
+      elements.msgList:insert(
+        ("[" .. ("%10.2f"):format(comp.uptime()) .. "] #" .. ("%5d"):format(port) ..
+        " " .. modem.address:sub(1, 8) .. "… → BROADCAST"),
+        {"BROADCAST", modem.address, port, distance, table.unpack(args)})
+      msgInfo:show()
+      updateMsgData()
+      return table.unpack(result)
+    end
+  end
+  return invoke(address, method, ...)
+end
+
 forms.run(main)
+
+com.invoke = invoke
+
 event.ignore("modem_message", modemListener)
 gpu.setResolution(oldW, oldH)
 gpu.setForeground(oldFG)
